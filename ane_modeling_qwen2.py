1a43f6f50148ba27957399fde39ba16a2d0cd57b
Nathan Ranchin <nathan.ranchin@epfl.ch>
2024-04-06 18:36:16 +0200

    Qwen2RMSNorm and Qwen2MLP done

--- ane_modeling_qwen2.py
+++ ane_modeling_qwen2.py
@@ -1,54 +1,85 @@
 import torch
-from torch import nn
+from typing import Tuple
 from transformers import Qwen2Config
 from argmaxtools import nn as ann, utils
+from torch import nn, Tensor, functional as F
+
+class Qwen2RMSNorm(nn.Module):
+    def __init__(self, config: Qwen2Config) -> None:
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        self.rms_norm_eps = config.rms_norm_eps
+        self.weight = nn.Parameter(torch.ones(self.hidden_size))
+
+    def forward(self, hidden_states: Tensor) -> Tensor:
+        # hidden_states: (batch_size, hidden_size, 1, seq_len)
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(dim=1, keepdim=True) # var on hidden_size => dim is 1 instead of -1
+        hidden_states *= torch.rsqrt(variance + self.rms_norm_eps)
+        return self.weight.view(1, self.hidden_size, 1, 1) * hidden_states.to(input_dtype)
 
 class Qwen2RotaryEmbedding(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
         super().__init__()
-
-class Qwen2RMSNorm(nn.Module):
-    pass
+    
+    def forward(self, query: Tensor, key: Tensor, position_ids: Tensor) -> Tuple[Tensor, Tensor]:
+        # query: (batch_size, hidden_size, 1, seq_len)
+        # key: (batch_size, hidden_size, 1, seq_len)
+        # position_ids: (batch_size, seq_len)
+        pass
 
 class Qwen2Attention(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
         super().__init__()
-        self.hidden_size = config.hidden_size
-        self.num_heads = config.num_attention_heads
-        self.head_dim = self.hidden_size // self.num_heads
-        self.num_key_value_heads = config.num_key_value_heads
 
-        self.q_proj = nn.Conv2d(config.hidden_size, self.num_heads * self.head_dim, 1, bias=True)
-        self.k_proj = nn.Conv2d(config.hidden_size, self.num_key_value_heads * self.head_dim, 1, bias=True)
-        self.v_proj = nn.Conv2d(config.hidden_size, self.num_key_value_heads * self.head_dim, 1, bias=True)
-        self.out_proj = nn.Conv2d(self.num_heads * self.head_dim, config.hidden_size, 1, bias=False)
-
-        self.rotary_emb = Qwen2RotaryEmbedding(config)
+    def forward(self, hidden_states: Tensor, attention_mask: Tensor, position_ids: Tensor, past_kv_values: Tensor) -> Tuple[Tensor, Tensor]:
+        # hidden_states: (batch_size, hidden_size, 1, seq_len)
+        # attention_mask: (batch_size, seq_len)
+        # position_ids: (batch_size, seq_len)
+        # past_kv_values: (2, batch_size, past_seq_len, hidden_size)
+        pass
 
 class Qwen2MLP(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
+        super().__init__()
         self.gate_proj = nn.Conv2d(config.hidden_size, config.intermediate_size, 1, bias=False)
         self.up_proj = nn.Conv2d(config.hidden_size, config.intermediate_size, 1, bias=False)
         self.down_proj = nn.Conv2d(config.intermediate_size, config.hidden_size, 1, bias=False)
 
+    def forward(self, hidden_states: Tensor) -> Tensor:
+        # hidden_states: (batch_size, hidden_size, 1, seq_len)
+        return self.down_proj(F.silu(self.gate_proj(hidden_states)) * self.up_proj(hidden_states))
+
 class Qwen2DecoderLayer(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
-        self.self_attn = Qwen2Attention(config)
-        self.mlp = Qwen2MLP(config)
-        self.input_layernorm = Qwen2RMSNorm(config)
-        self.post_attention_layernorm = Qwen2RMSNorm(config)
+        super().__init__()
+
+    def forward(self, hidden_states: Tensor, attention_mask: Tensor, position_ids: Tensor, past_kv_values: Tensor) -> Tuple[Tensor, Tensor]:
+        # hidden_states: (batch_size, hidden_size, 1, seq_len)
+        # attention_mask: (batch_size, seq_len)
+        # position_ids: (batch_size, seq_len)
+        # past_kv_values: (2, batch_size, past_seq_len, hidden_size)
+        pass
 
 class Qwen2Model(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
         super().__init__()
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size)
-        self.layers = nn.ModuleList([Qwen2DecoderLayer(config) for _ in range(config.num_hidden_layers)])
-        self._attn_implementation = config._attn_implementation
-        self.norm = Qwen2RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(self, input_ids: Tensor, attention_mask: Tensor, position_ids: Tensor, past_kv_values: Tensor) -> Tuple[Tensor, Tensor]:
+        # input_ids: (batch_size, seq_len)
+        # attention_mask: (batch_size, seq_len)
+        # position_ids: (batch_size, seq_len)
+        # past_kv_values: (num_hidden_layers, 2, batch_size, past_seq_len, hidden_size)
+        pass
 
 class Qwen2ForCausalLM(nn.Module):
     def __init__(self, config: Qwen2Config) -> None:
         super().__init__()
-        self.model = Qwen2Model(config)
-        self.vocab_size = config.vocab_size
-        self.lm_head = nn.Conv2d(config.hidden_size, config.vocab_size, 1, bias=False)
+
+    def forward(self, input_ids: Tensor, attention_mask: Tensor, position_ids: Tensor, past_kv_values: Tensor) -> Tuple[Tensor, Tensor]:
+        # input_ids: (batch_size, seq_len)
+        # attention_mask: (batch_size, seq_len)
+        # position_ids: (batch_size, seq_len)
+        # past_kv_values: (num_hidden_layers, 2, batch_size, past_seq_len, hidden_size)
+        pass
